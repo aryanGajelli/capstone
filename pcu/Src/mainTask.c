a012b9cd53ca769dd0c6585c4a90d4948419cff3
aryanGajelli <argajelli@gmail.com>
2024-11-16 18:17:25 -0500

    HSI instead HSE (xtal doesn't seem to be working)

--- pcu/Src/mainTask.c
+++ pcu/Src/mainTask.c
@@ -5,9 +5,9 @@
 
 #define MAIN_TASK_PERIOD_MS 500
 void mainTask(void const* argument) {
+    uprintf("Starting mainTask\n");
     TickType_t xLastWakeTime = xTaskGetTickCount();
     while (1) {
-        uprintf("Hello from mainTask\n");
         HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(MAIN_TASK_PERIOD_MS));
     }
