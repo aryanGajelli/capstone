94e5d59d3002ba8c68a043b1ee8748c3bb274dc0
aryanGajelli <argajelli@gmail.com>
2024-11-23 21:26:28 -0500

    No control required, bldc esc handles closed loop control

--- pcu/Inc/motor.h
+++ pcu/Inc/motor.h
@@ -3,17 +3,22 @@
 
 #include "stm32f4xx_hal.h"
 
-#define PPM_FREQ_HZ 50.
-#define PPM_PERIOD_US (1000000./PPM_FREQ_HZ)
+#define PPM_FREQ_HZ 50
+#define PPM_PERIOD_US (1000000/PPM_FREQ_HZ)
 #define MIN_PULSE_WIDTH_US 1560.
 #define MAX_PULSE_WIDTH_US 2350.
 
 #define MIN_DUTY_CYCLE (MIN_PULSE_WIDTH_US/PPM_PERIOD_US)
 #define MAX_DUTY_CYCLE (MAX_PULSE_WIDTH_US/PPM_PERIOD_US)
 
+#define getCurrentPulseWidthUS() __HAL_TIM_GET_COMPARE(&MOTOR_TIM_HANDLE, MOTOR_TIM_CHANNEL)
+
+
+
 HAL_StatusTypeDef motorInit(void);
-HAL_StatusTypeDef motorSetSpeed(float speed);
-HAL_StatusTypeDef motorSetDutyCycle(float dutyCycle);
-HAL_StatusTypeDef motorSetPulseWidth(float pulseWidth);
+HAL_StatusTypeDef motorSetSpeed(double speed);
+HAL_StatusTypeDef motorSetRPM(double requestedRPM);
+HAL_StatusTypeDef motorSetDutyCycle(double dutyCycle);
+HAL_StatusTypeDef motorSetPulseWidth(double pulseWidth);
 
 #endif // __MOTOR_H__
--- pcu/Src/motor.c
+++ pcu/Src/motor.c
@@ -6,9 +6,30 @@
 #include "stm32f4xx_hal.h"
 #include "tim.h"
 
+uint16_t PW_2_RPM_LUT[][2] = {
+    {1565, 463},
+    {1567, 500},
+    {1570, 530},
+    {1575, 639},
+    {1580, 745},
+    {1585, 850},
+    {1590, 925},
+    {1595, 1005},
+    {1600, 1105},
+    {1610, 1260},
+    {1650, 1805},
+    {1700, 2224},
+    {1750, 2505},
+    {1800, 2650},
+    {1850, 2784},
+    {1900, 2860},
+    {1950, 2950},
+    {2000, 3085}
+};
+
 uint32_t motorARR;
 HAL_StatusTypeDef motorInit() {
-    motorARR = __HAL_TIM_GET_AUTORELOAD(&MOTOR_TIM_HANDLE);
+    motorARR = __HAL_TIM_GET_AUTORELOAD(&MOTOR_TIM_HANDLE) + 1;
     if (HAL_TIM_PWM_Start(&MOTOR_TIM_HANDLE, MOTOR_TIM_CHANNEL) != HAL_OK) {
         return HAL_ERROR;
     }
@@ -16,20 +37,20 @@ HAL_StatusTypeDef motorInit() {
     return HAL_OK;
 }
 
-float map(float x, float in_min, float in_max, float out_min, float out_max) {
+double map(double x, double in_min, double in_max, double out_min, double out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-HAL_StatusTypeDef motorSetSpeed(float speed) {
+HAL_StatusTypeDef motorSetSpeed(double speed) {
     if (speed < 0 || speed > 100) {
         uprintf("Invalid speed: %f\n", speed);
         return HAL_ERROR;
     }
-    float pulseWidth = map(speed, 0, 100, MIN_PULSE_WIDTH_US, MAX_PULSE_WIDTH_US);
+    double pulseWidth = map(speed, 0, 100, MIN_PULSE_WIDTH_US, MAX_PULSE_WIDTH_US);
     return motorSetPulseWidth(pulseWidth);
 }
 
-HAL_StatusTypeDef motorSetDutyCycle(float dutyCycle) {
+HAL_StatusTypeDef motorSetDutyCycle(double dutyCycle) {
     if (dutyCycle < 0 || dutyCycle > 100) {
         uprintf("Invalid duty cycle: %f\n", dutyCycle);
         return HAL_ERROR;
@@ -40,13 +61,29 @@ HAL_StatusTypeDef motorSetDutyCycle(float dutyCycle) {
     return HAL_OK;
 }
 
-HAL_StatusTypeDef motorSetPulseWidth(float pulseWidth){
+HAL_StatusTypeDef motorSetRPM(double requestedRPM) {
+    if (requestedRPM < PW_2_RPM_LUT[0][1]){
+        uprintf("Invalid RPM: %f, min reqd: %u\n", requestedRPM, PW_2_RPM_LUT[0][1]);
+        return HAL_ERROR;
+    }
+    // linear interpolate with LUT
+    double pw = 0;
+    for (int i = 0; i < sizeof(PW_2_RPM_LUT) / sizeof(PW_2_RPM_LUT[0]); i++) {
+        if (requestedRPM < PW_2_RPM_LUT[i][1]) {
+            pw = map(requestedRPM, PW_2_RPM_LUT[i - 1][1], PW_2_RPM_LUT[i][1], PW_2_RPM_LUT[i - 1][0], PW_2_RPM_LUT[i][0]);
+            break;
+        }
+    }
+    return motorSetPulseWidth(pw);
+}
+
+HAL_StatusTypeDef motorSetPulseWidth(double pulseWidth) {
     // if (pulseWidth < MIN_PULSE_WIDTH_US || pulseWidth > MAX_PULSE_WIDTH_US) {
     //     uprintf("Invalid pulse width: %f\n", pulseWidth);
     //     return HAL_ERROR;
     // }
 
-    uint32_t CRR = pulseWidth / PPM_PERIOD_US * motorARR;
+    uint32_t CRR = pulseWidth * motorARR / PPM_PERIOD_US;
     __HAL_TIM_SET_COMPARE(&MOTOR_TIM_HANDLE, MOTOR_TIM_CHANNEL, CRR);
     return HAL_OK;
 }
