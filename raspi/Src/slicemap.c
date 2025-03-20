33dc216ac1f270594d4cf6ca5bd9772bf0815b6c
aryanGajelli <argajelli@gmail.com>
2025-03-20 17:18:48 -0400

    flipping x axis of back panel

--- raspi/Src/slicemap.c
+++ raspi/Src/slicemap.c
@@ -23,8 +23,11 @@ void slicemap_init() {
         // do lookup
         for (int panel = 0; panel < PANEL_COUNT; panel++) {
             for (int i_w = 0; i_w < PANEL_WIDTH; i_w++) {
+                int acc_w = panel == 0 ? PANEL_WIDTH - i_w : i_w;
+
                 // Position of point in frame p
-                double x_p = i_w - PANEL_WIDTH / 2;
+                double x_p = acc_w - PANEL_WIDTH / 2;
+                
 
                 // Position of panel in frame r
                 double p_x = -offsets[panel] * sin_angle;
@@ -37,7 +40,7 @@ void slicemap_init() {
                 // Convert rectangular coordinate to index
                 int index_x = clampf(x_r + PANEL_WIDTH / 2, 0, PANEL_WIDTH - 1);
                 int index_y = clampf(y_r + PANEL_WIDTH / 2, 0, PANEL_WIDTH - 1);
-
+                
                 slice_map[slice][i_w][panel] = (voxel_2D_t){index_x, index_y};
             }
         }
