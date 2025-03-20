04a7fdd04206868d4a7f0ae15d3b9960af63ebef
Anthony Bertnyk <anthony.bertnyk@gmail.com>
2025-03-19 20:17:27 -0400

    Prototype slicemap init

--- raspi/Src/slicemap.c
+++ raspi/Src/slicemap.c
@@ -19,6 +19,53 @@ void slicemap_init() {
         double angle = SLICE_2_RAD(slice);
         double sin_angle = sin(angle);
         double cos_angle = cos(angle);
+
         // do lookup
+
+        int i_p = 0;  // Front panel
+        double d = 0; // Offset
+        for (int i_w = 0; i_w < PANEL_WIDTH; i_w++)
+        {
+            // Position of point in frame p
+            double x_p = i_w - PANEL_WIDTH/2;
+
+            // Position of panel in frame r
+            double p_x = -d*sin_angle;
+            double p_y = d*cos_angle;
+
+            // Position of point in frame r
+            double x_r = x_p*cos_angle + p_x;
+            double y_r = x_p*sin_angle + p_y;
+
+            // Convert rectangular coordinate to index
+            int index_x = x_r + PANEL_WIDTH/2;
+            int index_y = y_r + PANEL_WIDTH/2;
+
+            slice_map[slice][i_w][i_p].x = index_x;
+            slice_map[slice][i_w][i_p].y = index_y;
+        }
+
+        i_p = 1; // Back panel
+        d = PANEL_BACK_OFFSET;  // Offset
+        for (int i_w = 0; i_w < PANEL_WIDTH; i_w++)
+        {
+            // Position of point in frame p
+            double x_p = i_w - PANEL_WIDTH/2;
+
+            // Position of panel in frame r
+            double p_x = -d*sin_angle;
+            double p_y = d*cos_angle;
+
+            // Position of point in frame r
+            double x_r = x_p*cos_angle + p_x;
+            double y_r = x_p*sin_angle + p_y;
+
+            // Convert rectangular coordinate to index
+            int index_x = x_r + PANEL_WIDTH/2;
+            int index_y = y_r + PANEL_WIDTH/2;
+
+            slice_map[slice][i_w][i_p].x = index_x;
+            slice_map[slice][i_w][i_p].y = index_y;
+        }
     }
 }
