60d20ae6762edf4a5a20245d7661e892313da56c
Philip Zhu <philipchu888@gmail.com>
2025-03-19 23:43:07 -0400

    load xyzrgb files

--- raspi/Inc/volumetric.h
+++ raspi/Inc/volumetric.h
@@ -5,4 +5,6 @@
 
 int volumetric_test(int argc, char **argv);
 uint32_t read_gpio(volatile uint32_t *read_reg, int bit);
+pixel_t rgb_to_8bit(unsigned char r, unsigned char g, unsigned char b);
+void populate_volume(FILE *file);
 #endif  // __VOLUMETRIC_H__
--- raspi/Src/voxel.c
+++ raspi/Src/voxel.c
@@ -4,6 +4,21 @@
 
 pixel_t volume[VOXELS_Z][VOXELS_Y][VOXELS_X];
 
+pixel_t rgb_to_8bit(unsigned char r, unsigned char g, unsigned char b) {
+    unsigned char r_3bits = (r*7+127) / 255;
+    unsigned char g_3bits = (g*7+127) / 255;
+    unsigned char b_2bits = (b*3+127) / 255;
+    return (r_3bits << 5) | (g_3bits << 2) | b_2bits;
+}
+
+void populate_volume(FILE *file) {
+    int x, y, z, r, g, b;
+    while (fscanf(file, "%d %d %d %d %d %d", &x, &y, &z, &r, &g, &b) != EOF) {
+        volume[z][y + PANEL_WIDTH / 2][x + PANEL_WIDTH / 2] = rgb_to_8bit(r, g, b);
+    }
+    fclose(file);
+}
+
 void volume_draw_plane() {
     printf("Filling cartesian volume buffer \n");
     for (int z = 0; z < VOXELS_Z; z++) {
